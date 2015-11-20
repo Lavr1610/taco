0d74c2ad7b51486ee20cb2f069287960e0f8a2ed
Tarasova <ehlina-tarasova@mail.ru>
2015-11-20 12:18:36 +0900

    Login and MainWindow added.

---
+++ src/MainWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by User13 on 30.10.2015.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        super();
+
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
